99118996785eb0e1040eb43ed3cef62b76b475dd
rvazquezglez <rvazquezglez@gmail.com>
2015-09-10 19:02:06 -0700

    Set values of right combo based on selection

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+var dias = {"1": "Lunes",
+  "2": "Martes",
+  "3": "Miercoles"
+};
+
+var meses = {"enero": "Enero",
+  "febrero": "Febrero",
+  "marzo": "Marzo"
+};
+
+function change_right_combo_values (type_values) {
+	var $el = $("#right_combo");
+	$el.empty();
+	$.each(type_values, function(value,key) {
+	  $el.append($("<option></option>")
+	     .attr("value", value).text(key));
+	});
+}
+
 $('#input_type').on('change', function() {
-  alert( $(this).val() );
+  if($(this).val() == 1 ){
+  	change_right_combo_values(dias);
+  } else if($(this).val() == 2 ){
+	change_right_combo_values(meses);
+  }
 });
